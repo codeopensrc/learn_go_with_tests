531c6a4bf7e36559da529b5f3f79af85db2628b7
Casey Jones <kcjones.2002@gmail.com>
2024-05-23 16:05:52 -0700

    arr and slice sumall start

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,16 +3,6 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
-    t.Run("collection of 5 numbers", func(t *testing.T) {
-        numbers := []int{1, 2, 3, 4, 5}
-        got := Sum(numbers)
-        expected := 15
-
-        if got != expected {
-            t.Errorf("expected %d but got %d given %v", expected, got, numbers)
-        }
-    })
-
     t.Run("collection of any size", func(t *testing.T) {
         numbers := []int{1, 2, 3}
         got := Sum(numbers)
@@ -23,4 +13,15 @@ func TestSum(t *testing.T) {
         }
     })
 
+}
+
+func TestSumAll(t *testing.T) {
+    //t.Run("collection of any size", func(t *testing.T) {
+        got := SumAll([]int{1, 2}, []int{0, 9})
+        expected := []int{3, 9}
+
+        if got != expected {
+            t.Errorf("expected %v but got %v", expected, got)
+        }
+    //})
 }
