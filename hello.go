1df6250f494a14465ff2db2c88b53c9b28401789
Casey Jones <kcjones.2002@gmail.com>
2024-05-23 11:55:09 -0700

    const and return vals

--- hello.go
+++ hello.go
@@ -2,24 +2,31 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const (
+    englishHelloPrefix = "Hello, "
+    spanishHelloPrefix = "Hola, "
+    frenchHelloPrefix = "Bonjour, "
+)
 
 func Hello(name string, lang string) string {
     if name == "" {
         name = "World"
     }
 
-    prefix := englishHelloPrefix
+    return greetingPrefix(lang) + name
+}
+
+func greetingPrefix(lang string) (prefix string) {
 
     switch lang {
         case "Spanish":
             prefix = spanishHelloPrefix
         case "French":
             prefix = frenchHelloPrefix
+        default:
+            prefix = englishHelloPrefix
     }
-    return prefix + name
+    return
 }
 
 func main() {
