58f9f93846dbf047f2f893f9f90bca58d98b6a3d
Casey Jones <kcjones.2002@gmail.com>
2024-05-24 10:29:24 -0700

    local funcs, slices

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -16,13 +16,23 @@ func TestSum(t *testing.T) {
 
 }
 
-func TestSumAll(t *testing.T) {
-    //t.Run("collection of any size", func(t *testing.T) {
-        got := SumAll([]int{1, 2}, []int{0, 9})
-        expected := []int{3, 9}
+func TestSumAllTails(t *testing.T) {
 
+    checkSums := func(t testing.TB, got, expected []int) {
+        t.Helper()
         if !reflect.DeepEqual(got, expected) {
             t.Errorf("expected %v but got %v", expected, got)
         }
-    //})
+    }
+
+    t.Run("make the sum of some slices", func(t *testing.T) {
+        got := SumAllTails([]int{1, 2}, []int{0, 9})
+        expected := []int{2, 9}
+        checkSums(t, got, expected)
+    })
+    t.Run("safely sum empty slices", func(t *testing.T) {
+        got := SumAllTails([]int{}, []int{3, 4, 5})
+        expected := []int{0, 9}
+        checkSums(t, got, expected)
+    })
 }
